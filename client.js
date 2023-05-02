8f24ea22d02b21c5ed0dc2e1ce3468b3ca143cdb
Hamza Muhumed <hamzamuhumed2107@gmail.com>
2023-05-02 10:21:45 -0500

    Completed assignment

--- client.js
+++ client.js
@@ -5,13 +5,17 @@ You’ve been given an index.html file and this starter js file.
   -- There is no JS to write for this step.
 */
 
-console.log('Step 0 complete! client.js loaded!');
-            
+console.log("Step 0 complete! client.js loaded!");
+
 // Write your code here!
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
 
+let highScore = 29;
+
 // 2. Make a new empty array called myScrabbleTiles.
+
+let myScrabbleTiles = [];
 
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
@@ -22,26 +26,61 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
+function addMyScrabbleTiles(tile, score) {
+  let scrabble = {
+    tile: tile,
+    score: score,
+  };
+  myScrabbleTiles.push(scrabble);
+  return scrabble;
+}
+
+console.log(addMyScrabbleTiles("N", 1));
+console.log(addMyScrabbleTiles("K", 5));
+console.log(addMyScrabbleTiles("Z", 10));
+console.log(addMyScrabbleTiles("X", 8));
+console.log(addMyScrabbleTiles("D", 2));
+console.log(addMyScrabbleTiles("A", 1));
 
 // 4. Remove the last tile from myScrabbleTiles.
-
+myScrabbleTiles.pop();
+console.log("Removed last tile:", myScrabbleTiles);
 
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
-
+myScrabbleTiles.push({ tile: "F", score: 4 });
+console.log("Added tile F", myScrabbleTiles);
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
-function sumTiles(anArray) {
-
+function sumTiles(myScrabbleTiles) {
+  let sumOf = 0;
+  for (let i = 0; i < myScrabbleTiles.length; i++) {
+    sumOf += myScrabbleTiles[i].score;
+  }
+  return sumOf;
 }
 
+let totalscore = sumTiles(myScrabbleTiles);
+console.log('The total score is ' , totalscore);
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-
+let myscore = totalscore;
+console.log('Therefore my score is ' , myscore);
 
 // 8. Check whether or not your score is higher than the highScore.
 // 8-1 If your score is higher, change highScore to the new high score.
+if (myscore > highScore) {
+  let myScore = highScore
+  console.log(myscore, "which is my score is higher than the high score of", highScore);
+} else {
+  console.log('Whew, my score of ' + myscore + 'is not higher than the high score which is ' + highScore);
+}; 
+
+
+
+// used extension Prettier to clean my code. 
 
 
 // DONE!
+
